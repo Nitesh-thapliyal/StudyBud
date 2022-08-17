3559afdbacdfe33e9501c2f657a7ab319689d02d
Nitesh Thapliyal <niteshnits.thapliyal@gmail.com>
2022-08-17 23:19:19 +0530

    update

--- base/views.py
+++ base/views.py
@@ -11,6 +11,10 @@ from .forms import RoomForm
 # ]
 
 
+def loginPage(request):
+    context = {}
+    return render(request, 'base/login_register.html',context)
+
 def home(request):
     #search functionality
     q = request.GET.get('q') if request.GET.get('q') != None else ''
