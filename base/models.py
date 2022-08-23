d279c5b9b866a72a4dd65ae7123916dda811e91b
Nitesh Thapliyal <niteshnits.thapliyal@gmail.com>
2022-08-23 22:24:59 +0530

    final

--- base/models.py
+++ base/models.py
@@ -1,8 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-# Database creation happens here
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+
+    avatar = models.ImageField(null=True, default="avatar.svg")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=200)
@@ -11,21 +20,18 @@ class Topic(models.Model):
         return self.name
 
 
-# class name represent table name and variable inside it are columns in table
-# models by default has id generated for them 
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # participants
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
 
-    # below class specify that the newest room should be displayed first in the home page
     class Meta:
-        ordering = ['-updated', 'created'] #here dash is for newest item first that is descending order
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.name
@@ -33,12 +39,13 @@ class Room(models.Model):
 
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # one to many relationship
-    room = models.ForeignKey(Room, on_delete=models.CASCADE) #here casecade specify that if the room gets deleted then all its children also gets deleted
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.body[0:50]
--- base/views.py
+++ base/views.py
@@ -1,47 +1,45 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required  # to restrict the pages
-from django.db.models import Q # it helps to provide the funcitonality of or and in our code
-from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
-from .models import Room,Topic,Message
-from .forms import RoomForm
+from .models import Room, Topic, Message, User
+from .forms import RoomForm, UserForm, MyUserCreationForm
+
 # Create your views here.
 
 # rooms = [
-#     {'id':1, 'name':'lets learn python'},
-#     {'id':2, 'name':'Backend Development'},
-#     {'id':3, 'name':'Frontend Development'},
+#     {'id': 1, 'name': 'Lets learn python!'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend developers'},
 # ]
 
 
 def loginPage(request):
-    page='login'
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username').lower()
+        email = request.POST.get('email').lower()
         password = request.POST.get('password')
 
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=email)
         except:
-            messages.error(request, 'User Not Found!')
+            messages.error(request, 'User does not exist')
 
-        user = authenticate(request, username = username, password=password)
+        user = authenticate(request, email=email, password=password)
 
         if user is not None:
             login(request, user)
             return redirect('home')
-
         else:
-            messages.error(request, 'Username OR password does not exist')
+            messages.error(request, 'Username OR password does not exit')
 
     context = {'page': page}
-    return render(request, 'base/login_register.html',context)
+    return render(request, 'base/login_register.html', context)
 
 
 def logoutUser(request):
@@ -50,10 +48,10 @@ def logoutUser(request):
 
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = MyUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -61,94 +59,146 @@ def registerPage(request):
             login(request, user)
             return redirect('home')
         else:
-            messages.error(request, 'An error occured during registration')
+            messages.error(request, 'An error occurred during registration')
 
     return render(request, 'base/login_register.html', {'form': form})
 
+
 def home(request):
-    #search functionality
     q = request.GET.get('q') if request.GET.get('q') != None else ''
+
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) |
         Q(name__icontains=q) |
         Q(description__icontains=q)
-        ) 
-    # here icontains means case insensitive
-    #here objects is a model manager or modle object variable
-    #now it is connected to db, we are going to see items from db
-    
-    topics = Topic.objects.all()
+    )
+
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
-    context ={'rooms':rooms, 'topics': topics, 'room_count':room_count }
-    return render(request, 'base/home.html',context)
+    room_messages = Message.objects.filter(
+        Q(room__topic__name__icontains=q))[0:3]
 
-def room(request,pk):
-    # room=None
-    # for i in rooms:
-    #     if i['id'] == int(pk):
-    #         room=i
-    # for being more specific after cliking the bakend develeopment, bakend development must open
+    context = {'rooms': rooms, 'topics': topics,
+               'room_count': room_count, 'room_messages': room_messages}
+    return render(request, 'base/home.html', context)
 
 
+def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created') #-created means descending order
-    #for sending message in the chat(conversation)
+    room_messages = room.message_set.all()
+    participants = room.participants.all()
+
     if request.method == 'POST':
         message = Message.objects.create(
-            user = request.user, 
-            room = room,
-            body = request.POST.get('body')
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
 
-    context={'room':room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages,
+               'participants': participants}
     return render(request, 'base/room.html', context)
 
-#decorator: now if the credentials are not matched then the user will be redirected to login page
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms,
+               'room_messages': room_messages, 'topics': topics}
+    return render(request, 'base/profile.html', context)
+
+
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        #print(request.POST)
-         form = RoomForm(request.POST)
-         if form.is_valid():
-             form.save()
-             return redirect('home') 
-    
-    context = {'form': form}
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+        )
+        return redirect('home')
+
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
-#for editing the room
+
 @login_required(login_url='login')
-def updateRoom(request,pk):
+def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
-    form = RoomForm(instance=room) # so that room value are already filled
-
-    #this will allow only the room owner to edit the room
+    form = RoomForm(instance=room)
+    topics = Topic.objects.all()
     if request.user != room.host:
-        return HttpResponse('Access Denied!!')
+        return HttpResponse('Your are not allowed here!!')
 
-
-    # processing the data:
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
 
-    context = {'form': form}
+    context = {'form': form, 'topics': topics, 'room': room}
     return render(request, 'base/room_form.html', context)
 
-# Deleting the item
+
 @login_required(login_url='login')
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
 
-    #this will allow only the room owner to edit the room
     if request.user != room.host:
-        return HttpResponse('Access Denied!!')
+        return HttpResponse('Your are not allowed here!!')
 
     if request.method == 'POST':
         room.delete()
         return redirect('home')
-    return render(request, 'base/delete.html', {'obj':room})
+    return render(request, 'base/delete.html', {'obj': room})
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('Your are not allowed here!!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': message})
+
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    return render(request, 'base/update-user.html', {'form': form})
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
+
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
